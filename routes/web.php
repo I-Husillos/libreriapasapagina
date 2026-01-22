3f0f3e373b0349d2f20d7c7658ce218922736dd3
I-Husillos <ivanhuslabra@gmail.com>
2026-01-22 09:37:43 +0100

    creamos los campos rellenabes del modelo Libro, los campos de la migración y el seeder de Libro

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::resource('libro', LibroController::class);
 
 // ruta para confirmar eliminacion
-Route::get('libros/{libro}/destroy/confirm', [LibroController::class, 'confirmDestroy'])->name('libro.confirmDestroy');
+Route::get('libro/{libro}/destroy/confirmDestroy', [LibroController::class, 'confirmDestroy'])->name('libro.confirmDestroy');
